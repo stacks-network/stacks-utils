9f45291b7e35afc78b8f598d4e1e279c7615e044
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-11-27 09:29:15 -0600

    get fees for transactions, makes decodeRawTx async

--- src/transactions.js
+++ src/transactions.js
@@ -4,12 +4,53 @@ import { b58ToC32 } from "c32check";
 import { microToStacks } from "./units";
 
 /**
+ * lookupValue
+ *
+ * @param {String} hashBuffer
+ * @param {Number} index
+ * @returns {Promise} the Number represents the value of each output of a given hash
+ */
+const lookupValue = async (hashBuffer, index) => {
+  try {
+    const txHash = Buffer.from(hashBuffer)
+      .reverse()
+      .toString("hex");
+    const response = await fetch(
+      `https://blockchain.info/rawtx/${txHash}?format=hex`
+    );
+    const rawTx = await response.text();
+    const tx = btc.Transaction.fromHex(rawTx);
+    const value = tx.outs[index].value;
+
+    return value;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+/**
+ * getFees
+ *
+ * @param {Object} tx - the BTC transaction
+ * @returns {Promise} the Number represents the difference of inputValues and outputValues (totalFeesPaid)
+ */
+const getFees = async tx => {
+  const inputValues = await Promise.all(
+    tx.ins.map(async x => lookupValue(x.hash, x.index))
+  ).then(results => results.reduce((a, b) => a + b, 0));
+  const outputValues = tx.outs.map(x => x.value).reduce((a, b) => a + b, 0);
+  const totalFeesPaid = inputValues - outputValues;
+  return totalFeesPaid;
+};
+
+/**
  * getOperationType
  *
  * Returns a readable operation type
  * see: https://docs.blockstack.org/core/wire-format.html
  *
  * @param {String} opCode - the ascii character
+ * @returns {String} operation - the readable operation for a given opCode
  */
 const getOperationType = opCode => {
   if (opCode === "$") {
@@ -45,6 +86,7 @@ const getOperationType = opCode => {
   if (opCode === "!") {
     return "NAMESPACE_READY";
   }
+  console.error("stacks-utils: getOperationType -- Unknown Stacks Operation");
   return null;
 };
 
@@ -55,11 +97,13 @@ const getOperationType = opCode => {
  * and provide stacks transaction information.
  *
  * @param {String} rawTx - the hex tx to decode
+ * @returns {Promise} tx - the decompiled Stacks transaction
  */
-const decodeRawTx = rawTx => {
+const decodeRawTx = async rawTx => {
   const tx = btc.Transaction.fromHex(rawTx);
   const data = btc.script.decompile(tx.outs[0].script)[1];
   if (!data.slice) {
+    console.error("stacks-utils: decodeRawTx -- cannot slice data.");
     return;
   }
   const operationType = data.slice(2, 3).toString();
@@ -94,6 +138,9 @@ const decodeRawTx = rawTx => {
   const senderBitcoinAddress = btc.address.toBase58Check(hash, version);
   const senderStacksAddress = b58ToC32(senderBitcoinAddress);
 
+  // fetch our fees
+  const fees = await getFees(tx);
+
   return {
     sender: senderStacksAddress,
     senderBitcoinAddress,
@@ -105,7 +152,8 @@ const decodeRawTx = rawTx => {
     tokenType,
     tokenAmount: tokenSentBigI,
     tokenAmountReadable: microToStacks(tokenSentBigI.toString()),
-    memo: scratchData
+    memo: scratchData,
+    fees
   };
 };
 
